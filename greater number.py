3a48a200aeffe7b4b82627c8d65c3902ab4e65f0
Navyapagidimarri <navyap.marolix@gmail.com>
2023-07-31 10:40:15 +0530

    my third commit

---
+++ greater number.py
@@ -0,0 +1,8 @@
+n=int(input("Enter Number:"))
+m=int(input("Enter Number:"))
+if ( n>m ):
+    print("Greatest Number :  n")
+elif ( m>n ):
+    print("Greatest Number :m")
+else:
+  print("Both are Equal")
