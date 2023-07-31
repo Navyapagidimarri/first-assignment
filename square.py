88288bbe27339d1f0a107c5a97867c2be9d944fe
Navyapagidimarri <navyap.marolix@gmail.com>
2023-07-31 12:09:31 +0530

    check its square or not

---
+++ square.py
@@ -0,0 +1,7 @@
+length=int(input("enter lenth:"))
+breadth=int(input("enter breadth:"))
+#measurements of square is length == breadth
+if (length==breadth):
+    print("its is a square")
+else:
+    print("its not a square")
