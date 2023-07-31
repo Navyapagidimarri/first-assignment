e7437e23db82df45dce4a6a7455383e727715927
Navyapagidimarri <navyap.marolix@gmail.com>
2023-07-31 11:56:15 +0530

    voting example

---
+++ vote.py
@@ -0,0 +1,6 @@
+age=int(input("Enter age to check eligabilty for vote or not:"))
+if age>=18:
+    print("Yes! Your eligable for voting")
+else:
+    print("no ,you are not eligable for voting")
+    
