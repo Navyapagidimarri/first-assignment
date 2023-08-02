793ae0f7e083488be60bfead0050b29fd1280b2f
Navyapagidimarri <navyap.marolix@gmail.com>
2023-08-02 10:28:56 +0530

    my first string

---
+++ string1.py
@@ -0,0 +1,9 @@
+#we maximum used object is string for all
+#we declare a string with ""
+
+i="navya"
+j='navya'
+print(i)
+print(j)
+
+#here one thing is to confirm here is we can write a string with '' also
