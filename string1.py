3761e46bacad2cd2c4cf604e3c2f04e7cbe66721
Navyapagidimarri <navyap.marolix@gmail.com>
2023-08-02 12:25:37 +0530

    string2

--- string1.py
+++ string1.py
@@ -1,9 +1,21 @@
 #we maximum used object is string for all
 #we declare a string with ""
 
-i="navya"
-j='navya'
-print(i)
-print(j)
+#i="navya"
+#j='navya'
+#print(i)
+#print(j)
 
 #here one thing is to confirm here is we can write a string with '' also
+
+#slicing
+#i="am working in marolix company"
+#print(i[:15])
+#print(i[4:10:-1])
+#print(i[:])
+
+#indexing
+i="am working in marolix company"
+print(i[10])
+
+
