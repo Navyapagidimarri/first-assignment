c292d350b76ca08ec92da56b1efe19408cf148d8
Navyapagidimarri <navyap.marolix@gmail.com>
2023-08-05 15:19:55 +0530

    vowel-consonant

---
+++ vowel -consonant.py
@@ -0,0 +1,8 @@
+s1=input("enter single alphabet:")
+
+if s1 in ("a","e","i","o","u","A","E","I","O","U"):
+    print("the entered alphabet is vowel")
+else:
+    print("the entered alphabet is consonant")
+
+
