6cb9946de64d88a72639de9ebf5db7f140271264
Navyapagidimarri <navyap.marolix@gmail.com>
2023-08-07 13:35:56 +0530

    myfile

--- countintstring.py
+++ countintstring.py
@@ -1,10 +1,14 @@
 s="aabbcc121122@@"
-count=0
+digitC=0
+alphaC=0
+specialC=0
 for i in s:
     if i.isdigit():
-     count=count+1
-print("number of digits",count)
-for k in s:
-    if i.isalpha():
-     count=count+1
-print("number of alphabets",count)
+     digitC=digitC+1
+    elif i.isalpha(): 
+     alphaC=alphaC+1
+    else:
+     specialC=specialC+1
+print("number of special charactors",specialC)
+print("number of digit",digitC)
+print("number of digit",alphaC)
