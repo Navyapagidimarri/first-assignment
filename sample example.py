0515ff9d473ba492f81f0fe5857ac4efc51dab16
Navyapagidimarri <navyap.marolix@gmail.com>
2023-08-16 13:18:59 +0530

    input method sum

--- sample example.py
+++ sample example.py
@@ -1,14 +1,15 @@
 # create a table with while loop and for loop
-l=int(input("enter table name"))
-i=1
-while(i<11):
-    print(l,'X',i ,'=',l*i)
-    i=i+1
+# l=int(input("enter table name"))
+# i=1
+# while(i<11):
+#     print(l,'X',i ,'=',l*i)
+#     i=i+1
 
 
 # l=int(input("enter table name"))
 # for i in range(1,11):
 #     print(l,'X',i ,'=',l*i)
+
 
 
 #program for interchange last 2 num in list
@@ -48,3 +49,22 @@ while(i<11):
 # return str1
 
 # print(change_char(restart))
+
+
+
+l1=eval(input("enter the size1:" ))
+l2=eval(input("enter the size2:"))
+new_list1=[]
+new_list2=[]
+sum_list=[]
+for i in range(0,l1):
+   element1=int(input("enter the element:"+str(i)+":"))
+   new_list1.append(element1)
+print(new_list1)
+for i in range(0,l2):
+  element2=int(input("enter the element:"+str(i)+":"))
+  new_list2.append(element2)
+print(new_list2)
+for i in range(len(new_list2)):
+  sum_list.append(new_list1[i]+new_list2[i])
+print(sum_list)
