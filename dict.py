6895e4674eb65b2bda3b2c20bbb88449d99dc2b0
Navyapagidimarri <navyap.marolix@gmail.com>
2023-08-24 10:03:15 +0530

    dict

--- dict.py
+++ dict.py
@@ -1,3 +1,16 @@
+#dict methods
+#length()
+# d={1:"white",2:"black",3:"blue"}
+# print(len(d))
+
+
+
+# d={1:"white",2:"black",3:"blue"}
+# print(d.get(4))#it will show none
+# print(d[4])#it will get key error
+
+
+
 #dict respresesntation in key:value
 # d={
 # "nursery":"g.Bhargav ram",
@@ -19,13 +32,21 @@
 # for y in d.values():
 #     print(y)
 
+
+# d={"boys":{"abhiram":"4thclass",
+#                "bhargavram":"nursery"},
+#    "girls":{"navya":"inter","smitha":"degree"}
+# }
+# print(d["girls"]["smitha"])
+
+
 d={"boys":{"abhiram":"4thclass",
                "bhargavram":"nursery"},
    "girls":{"navya":"inter","smitha":"degree"}
-}
-print(d["girls"]["smitha"])
-
-
-
+   }
+print(d.pop(0))
+print(d)
+#print(d.popitem())#it returns lat inserted index
+#print(d)
 
 
