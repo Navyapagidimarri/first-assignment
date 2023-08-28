fd550fced02b23637841e5523b519c3241c6a917
Navyapagidimarri <navyap.marolix@gmail.com>
2023-08-28 13:22:30 +0530

    myfun

--- def function.py
+++ def function.py
@@ -27,12 +27,53 @@
 # greet("marolix")
 
 
+
+
 #count area of a square
-def squareornot(length,breadth):
-#for sqaure lenth and breadth is same otherwise it's not square
-    if length==breadth:
-        print("this is a sqaure")
-    else:
-        print("no this is not a square")
-squareornot(20,20)
-squareornot(30,20)
+# def squareornot(length,breadth):
+# #for sqaure lenth and breadth is same otherwise it's not square
+#     if length==breadth:
+#         print("this is a sqaure")
+#     else:
+#         print("no this is not a square")
+# squareornot(20,20)
+# squareornot(30,20)
+
+
+
+
+#polindrome
+# def polindromeornot(x):
+#     if (x[::-1]==x):
+#         print("it's polindrome")
+#     else:
+#         print("it's not polindrome")
+# polindromeornot("madam")
+# polindromeornot("navya")
+
+
+
+
+#count 
+def countelements(s):
+  digitC=0
+alphaC=0
+specialC=0
+for i in s:
+  if i.isdigit():
+    digitC=digitC+1
+  elif i.isalpha(): 
+   alphaC=alphaC+1
+  else:
+   specialC=specialC+1
+print("number of special charactors",specialC)
+print("number of digit",digitC)
+print("number of digit",alphaC)
+countelements("navya0112233@!")
+
+
+
+
+
+     
+
