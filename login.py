03bdb4d0176afcc246667d6ba77c4f2ced5f458a
Navyapagidimarri <navyap.marolix@gmail.com>
2023-09-12 15:28:09 +0530

    login

--- login.py
+++ login.py
@@ -14,32 +14,53 @@
 #         print( "please sign up for new login")
 
 
+#login - new login
+# def login_page():
+#     d={
+#        "navya":"123456",
+#        "anusha":"234567",
+#        "madhugayathri":"567890",
+#        "rishika":"456789"
 
-def login_page():
-    d={
-       "navya":"123456",
-       "anusha":"234567",
-       "madhugayathri":"567890",
-       "rishika":"456789"
+#       }
+#     for key,value in d.items():
+#      key=input("enter a valid key:")
+#      value=input("enter a valid value")
+#     if d[key]==value:
+#      print("correct")
+#     else:
+#      print("please try again")
+#     for k in range(0,3):
+#       key=input("enter new username:")
+#       value=input("passwords:")
+#       d[key]=value
+#       print(d)
+#     enter_key=input("enter a key")
+#     enter_value=input("enter a value")
+#     if enter_key in d.keys():
+#      print("key already exists")
+#     else:
+#      d.update({enter_key:enter_value})
+#     print(d)
+# login_page()
 
-      }
-    for key,value in d.items():
-     key=input("enter a valid key:")
-     value=input("enter a valid value")
-    if d[key]==value:
-     print("correct")
-    else:
-     print("please try again")
-    for k in range(0,3):
-      key=input("enter new username:")
-      value=input("passwords:")
-      d[key]=value
-      print(d)
-    enter_key=input("enter a key")
-    enter_value=input("enter a value")
-    if enter_key in d.keys():
-     print("key already exists")
-    else:
-     d.update({enter_key:enter_value})
-    print(d)
-login_page()
+
+#register
+def register():
+    d={}
+    for i in range(0,2):
+          input_user=input("username")
+          input_password=input("password")
+          d[input_user]=input_password
+          print(d)
+def new_login():
+          for j in range(0,2):
+           check_enter_username=input("enter new username")
+           check_enter_password=input("enter new password")
+            d[check_enter_username]=check_enter_password
+           if check_enter_username in keys():
+                print("username is already exists")
+           else:
+             d.update({check_enter_username:check_enter_password})
+             print(d)
+register()
