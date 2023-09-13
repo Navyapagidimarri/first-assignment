98ecd86855c59eb9dfc17a739ed277e5c8c39970
Navyapagidimarri <navyap.marolix@gmail.com>
2023-09-13 14:46:03 +0530

    login

--- login.py
+++ login.py
@@ -46,11 +46,39 @@
 
 
 #register
-def register():
-    d={}
-    for i in range(0,2):
-          input_user=input("username")
-          input_password=input("password")
-          d[input_user]=input_password
-          print(d)
-register()
+# def register():
+#     d={}
+#     for i in range(0,2):
+#           input_user=input("username")
+#           input_password=input("password")
+#           d[input_user]=input_password
+#           print(d)
+# register()
+# def signup():
+#     for j in range(0,2):
+#         new_input_user=input("enter new username")
+#         if new_input_user==input_user:
+#             print("please enter new user")
+#             new_input_user1=input("enter new username")
+#         new_input_password=input("enter new password")
+#         new_input_confirm_password=input("enter password again")
+#         if new_input_password!=new_input_confirm_password:
+#             print("please enter again")
+#         else:
+#             d.update({new_input_user:new_input_confirm_password})
+#         d[new_input_user]=new_input_confirm_password
+#         print(d)
+#         print("all data registerrd successfully and")
+def login():
+    d = {'navya': '123456', 
+         'kavitha': '234567',
+          'anusha': '123456', 
+          'mamatha': '1111'}
+    for key,value in d.items():
+      login_name=input("enter user name")
+      login_passwod=input("enter input password")
+      if login_name in d.keys():
+          print("login successfully")
+      else:
+          print( "please sign up for new login")
+login()
