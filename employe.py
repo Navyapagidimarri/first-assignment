48765a4a935c5d8d1db81f3713c98f59a5c52506
Navyapagidimarri <navyap.marolix@gmail.com>
2023-09-20 10:29:18 +0530

    final domain-emp details code

--- employe.py
+++ employe.py
@@ -1,17 +1,62 @@
-def add_employee_details():
- d={}
- data=0
- entry_id=int(input("enter number of enteries"))
- for i in range(0,entry_id):
-  emp_id=input("enter employee id:")
-  if emp_id in d.keys():
-    print("employee already registerd ,please enter valid employee_id:")
-    emp_id=input("enter employee id:")
-  emp_name=input("enter employee name:")
-  emp_domain=input("enter eomployee  domain:")
-  emp_email=input("enter email id:")
-  d[emp_id]=emp_name,emp_email,emp_domain
-  print("employee details added successfully")
-  print(d)
-  data=data+1
-add_employee_details()
+#Adding all employee details to the dictinary 
+#def add_employee_details():
+#  d={}
+#  employ_entry=int(input("enter keys:"))
+#  for i in range(employ_entry):
+#   emp_id=input("enter employee id:")
+#   emp_name=input("enter employee name:")
+#   emp_domain=input("enter domain domain:")
+#   emp_email=input("enter email id:")
+#   d[emp_domain]=emp_name,emp_email,emp_id
+#   print(emp_domain,":",emp_name)
+# add_employee_details()
+
+
+# def employee_details():
+#     d = {}
+#     employ_entry = int(input("Enter the number of employes: "))
+#     for i in range(employ_entry):
+#         emp_name = input("Enter employee name: ")
+#         emp_domain = input("Enter domain: ")
+#         emp_id=input("enter employee id:")
+#         emp_email=input("enter email id:")
+#         d[emp_name]=emp_id,emp_email,emp_domain,emp_name
+#         print(d)
+#     print("Employee details:")
+#     print("*****************")
+#     for domain, name in d.items():
+#      if emp_domain in d:
+#         d[emp_domain].append(emp_name)
+#         print("Domain:", domain)
+#         print("Employee Names:", name)
+# employee_details()
+
+
+
+def employee_details():
+    d = {}  # Dictionary to store employee details by domain
+    employ_entry = int(input("Enter the number of employees: "))
+    for i in range(employ_entry):
+        emp_name = input("Enter employee name: ")
+        emp_domain = input("Enter domain: ")
+        emp_id = input("Enter employee id: ")
+        emp_email = input("Enter email id: ")
+        if emp_domain in d:
+            d[emp_domain].append((emp_name, emp_id, emp_email))
+        else:
+            d[emp_domain] = [(emp_name, emp_id, emp_email)]
+        print(d)
+    print("Employee details:")
+    print("*****************")
+    for domain, employees in d.items():
+        print("Domain:", domain)
+        print("Employee Details:")
+        for emp in employees:
+            emp_name, emp_id, emp_email = emp
+            print("Employee Name:", emp_name)
+            print("Employee ID:", emp_id)
+            print("Employee Email:", emp_email)
+
+employee_details()
+
+
---
+++ pattern.py
@@ -0,0 +1,9 @@
+#90 degrees Traingle in reverse way
+# for i in range(6,0,-1):
+#     print("*"*i)
+
+
+#90 degrees traingle in correct way
+# for i in range(1,6):
+#     print("*"*i)
+
